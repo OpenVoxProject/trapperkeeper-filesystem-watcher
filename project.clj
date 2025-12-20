5a31addd5b087f67692ad7918bb585d303a5003a
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-19 16:54:33 -0800

    Updates for OpenVox

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject puppetlabs/trapperkeeper-filesystem-watcher "1.2.7-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-filesystem-watcher "1.2.7-SNAPSHOT"
   :description "Trapperkeeper filesystem watcher service"
-  :url "https://github.com/puppetlabs/trapperkeeper-filesystem-watcher"
+  :url "https://github.com/openvoxproject/trapperkeeper-filesystem-watcher"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :min-lein-version "2.9.1"
 
-  :parent-project  {:coords [puppetlabs/clj-parent "7.3.20"]
+  :parent-project  {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
                     :inherit [:managed-dependencies]}
 
   :pedantic? :abort
@@ -15,28 +15,27 @@
                  [org.clojure/tools.logging]
                  [prismatic/schema]
                  [clj-commons/fs]
-                 [puppetlabs/trapperkeeper]
-                 [puppetlabs/kitchensink]
-                 [puppetlabs/i18n]]
+                 [org.openvoxproject/trapperkeeper]
+                 [org.openvoxproject/kitchensink]
+                 [org.openvoxproject/i18n]]
 
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_PASSWORD
                                      :sign-releases false}]]
 
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
 
-  :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper
+  :profiles {:dev {:dependencies [[org.openvoxproject/trapperkeeper
                                    :classifier "test"
                                    :scope "test"]
-                                  [puppetlabs/kitchensink
+                                  [org.openvoxproject/kitchensink
                                    :classifier "test"
                                    :scope "test"]]}}
 
   :plugins  [[lein-parent "0.3.7"]
              [jonase/eastwood "1.4.2"]
-             [puppetlabs/i18n "0.9.2"]]
+             [org.openvoxproject/i18n "0.9.2"]]
 
   :main puppetlabs.trapperkeeper.main)
-
