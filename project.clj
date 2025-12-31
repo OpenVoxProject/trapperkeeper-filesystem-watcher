ec1c62e84a5e338bc72eb0fa53f06d081b26dcc4
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 03:55:58 +0000

    Version 1.2.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/trapperkeeper-filesystem-watcher "1.2.7"
+(defproject org.openvoxproject/trapperkeeper-filesystem-watcher "1.2.8-SNAPSHOT"
   :description "Trapperkeeper filesystem watcher service"
   :url "https://github.com/openvoxproject/trapperkeeper-filesystem-watcher"
   :license {:name "Apache License, Version 2.0"
